64f6165545a94e61ad1ad04b2fc32848ef867ea1
MathildeJ <mathilde.jean@depinfonancy.net>
2016-08-30 11:47:00 +0200

    put back code snippet

--- static/splitscreen.js
+++ static/splitscreen.js
@@ -1,8 +1,3 @@
-(function(s,u,r,f,l,y){s[f]=s[f]||{init:function(){s[f].q=arguments}};
-l=u.createElement(r);y=u.getElementsByTagName(r)[0];l.async=1;
-l.src='https://surfly.com/surfly.js';y.parentNode.insertBefore(l,y);})
-(window,document,'script','Surfly');
-
 // navigate to a new url
 function navigate(){
 	var url = document.getElementById('url').value;
