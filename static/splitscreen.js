46a5cbda3acdbcfa8b52f7e5e4b96b702dfa05e9
MathildeJ <mathilde.jean@depinfonancy.net>
2016-08-30 14:16:08 +0200

    testing return button

--- static/splitscreen.js
+++ static/splitscreen.js
@@ -67,9 +67,10 @@ function previous_page(){
   console.log('referrer:' + document.referrer);
   console.log('frame history (should be >0): ' + frame.history.length);
   //frame.contentWindow.history.back();
+  console.log(urls.indexOf(lastUrl));
   var previous_url = urls[urls.indexOf(lastUrl)-1];
   console.log(previous_url);
-  sess.relocate(previous_url);  
+  //sess.relocate(previous_url);  
 }
 
 // ignore/acknowledge friend button
